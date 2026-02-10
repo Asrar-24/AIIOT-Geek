bbc9002fb6f0eaa3ccf3c1574ab986eff043a1d1
Asrar-24 <127003628+Asrar-24@users.noreply.github.com>
2026-02-10 23:01:01 +0530

    Update app.py

--- Basic-ML/House-Price-Prediction/app.py
+++ Basic-ML/House-Price-Prediction/app.py
@@ -5,25 +5,29 @@ import numpy as np
 import os
 
 
-# Base directory:
+# ---------------- PATH ----------------
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
-# Model & Feature Paths:
 model_path = os.path.join(BASE_DIR, "house_price_model.pkl")
 feature_path = os.path.join(BASE_DIR, "model_features.pkl")
 
 
-# Load
+# ---------------- LOAD ----------------
+
 model = joblib.load(model_path)
 features = joblib.load(feature_path)
 
 
-st.title("House Price Prediction")
+# ---------------- UI ----------------
+
+st.set_page_config(page_title="House Price Prediction")
+
+st.title("🏠 House Price Prediction")
 st.write("Predict House Price using ML")
 
 
-# USER INPUTS:
+# ---------------- INPUTS ----------------
 
 area = st.number_input("Area (sqft)", 500, 10000, 1500)
 bedrooms = st.number_input("Bedrooms", 1, 10, 3)
@@ -36,46 +40,37 @@ guestroom = st.selectbox("Guest Room", ["yes", "no"])
 basement = st.selectbox("Basement", ["yes", "no"])
 hotwaterheating = st.selectbox("Hot Water Heating", ["yes", "no"])
 airconditioning = st.selectbox("Air Conditioning", ["yes", "no"])
+prefarea = st.selectbox("Preferred Area", ["yes", "no"])
+
 furnishingstatus = st.selectbox(
     "Furnishing Status",
     ["furnished", "semi-furnished", "unfurnished"]
 )
 
 
-# PREDICTION Part:
+# ---------------- PREDICT ----------------
 
 if st.button("Predict Price"):
 
-# Create raw Dict input to convert into DF later:
     input_dict = {
         "area": area,
         "bedrooms": bedrooms,
         "bathrooms": bathrooms,
         "stories": stories,
         "parking": parking,
+
         "mainroad": mainroad,
         "guestroom": guestroom,
         "basement": basement,
         "hotwaterheating": hotwaterheating,
         "airconditioning": airconditioning,
+        "prefarea": prefarea,
+
         "furnishingstatus": furnishingstatus
     }
 
+
     df_input = pd.DataFrame([input_dict])
 
+
     # Encode like training
-    df_encoded = pd.get_dummies(df_input, drop_first=True)
-
-    # Add missing columns as 0 if missing.
-    for col in features:
-        if col not in df_encoded.columns:
-            df_encoded[col] = 0
-
-    # Reorder columns
-    df_encoded = df_encoded[features]
-
-    # Predict
-    log_price = model.predict(df_encoded)[0] # cause I used log transformation in Training.
-    price = np.exp(log_price)
-
-    st.success(f"Estimated Price: ₹ {int(price):,}")
